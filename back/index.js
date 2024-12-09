d3cc10cff891b9a537cb949a94d85ead40ea3e2a
FelipeYusti <felipe_yus@outlook.com>
2024-12-09 14:54:00 -0500

    Todo bien, Todo bonito...

--- back/index.js
+++ back/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 4100; // CONFIGURAMOS EL PUERTOs
+
+const { config } = require("dotenv");
 app.use(cors());
 app.use(express.json());
-
+const PORT = process.env.PORT || 4100; // CONFIGURAMOS EL PUERTOs
 app.use("/", require("./src/modulos/historial.js")); // traemos nuestra libreria historial de delitos
 app.use("/", require("./src/modulos/citizen.js")); // traemos nuestra libreria citizen.
 app.use("/", require("./src/modulos/delitos.js")); // traemos nuestra libreria delitos.
